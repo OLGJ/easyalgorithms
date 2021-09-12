b107d394cf890dee75a6b9cfc8cd85490a60666f
OLGJ <olof.josefsson@outlook.com>
2021-09-12 15:33:35 +0200

    Updates

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -9,6 +9,10 @@
 # - Graph: dataframe & contain columns ("v1", "v2", "w").
 # - init_node: be a numeric scalar.
 
+#wiki_graph <-
+#  data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#             v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#             w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
 
 dijkstra <-function(graph, init_node){
 
@@ -73,9 +77,6 @@ dijkstra <-function(graph, init_node){
   }
 
 }
-dijkstra(graph = wiki_graph, 1)
+#dijkstra(graph = wiki_graph, 1)
 
-wiki_graph <-
-  data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-             v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-             w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+
